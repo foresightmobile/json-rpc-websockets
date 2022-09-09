8999c46cdeec2cdc7f49508a4e24485be091b789
artemHrebinikChisw <75477796+artemHrebinikChisw@users.noreply.github.com>
2022-09-09 11:07:45 +0300

    added Client factory, added public ClientProvider interface, function access levels changed

--- Sources/JSONRPCWebSockets/Client.swift
+++ Sources/JSONRPCWebSockets/Client.swift
@@ -7,17 +7,36 @@
 
 import Foundation
 
-public class Client: NSObject {
+//MARK: - Client Interface
+public class Client {
+    public static func getClient() -> ClientProvider {
+       //TODO: - use property or constructor injectionf for websockets mechanism
+        return ClientImpl()
+    }
+}
+
+public protocol ClientProvider: AnyObject {
+    func connect(url: URL, queue: OperationQueue?, completion: @escaping () -> Void)
+    func disconnect(completion: @escaping () -> Void)
+    func notify<T: Codable>(method: String, parameters: T, completion: @escaping (Result<(), Error>) -> Void)
+    func call<T: Codable, U: Decodable>(method: String, parameters: T, type: U.Type, timeout: TimeInterval?, completion: @escaping (Result<U?, Error>) -> Void)
+    func subscribe<T: Codable>(to method: String, type: T.Type) throws
+    func on<T: Codable>(method: String, type: T.Type, completion: @escaping (T) -> Void)
+    func unsubscribe(from method: String)
+}
+
+class ClientImpl: NSObject, ClientProvider {
     private var urlSession: URLSession?
     private var webSocketTask: URLSessionWebSocketTask?
-
+    
     private var connectCompletion: (() -> Void)?
     private var disconnectCompletion: (() -> Void)?
     
     private var receivableSubscribers = [ReceivableSubscriber]()
     private var notificationSubscribers = [NotificationSubscriber]()
+    private let defaultInterval: TimeInterval = 5
     
-    public func connect(url: URL, queue: OperationQueue? = nil, completion: @escaping () -> Void) {
+    func connect(url: URL, queue: OperationQueue?, completion: @escaping () -> Void) {
         connectCompletion = completion
         
         urlSession = URLSession(configuration: .default, delegate: self, delegateQueue: queue)
@@ -28,13 +47,13 @@ public class Client: NSObject {
         ping()
     }
     
-    public func disconnect(completion: @escaping () -> Void) {
+    func disconnect(completion: @escaping () -> Void) {
         webSocketTask?.cancel(with: .goingAway, reason: nil)
         
         disconnectCompletion = completion
     }
     
-    public func notify<T: Codable>(method: String, parameters: T, completion: @escaping (Result<(), Error>) -> Void) {
+    func notify<T: Codable>(method: String, parameters: T, completion: @escaping (Result<(), Error>) -> Void) {
         let request = Request(id: nil, method: method, parameters: parameters)
         
         do {
@@ -57,7 +76,7 @@ public class Client: NSObject {
         }
     }
     
-    public func call<T: Codable, U: Decodable>(method: String, parameters: T, type: U.Type, timeout: TimeInterval = 5, completion: @escaping (Result<U?, Error>) -> Void) {
+    func call<T: Codable, U: Decodable>(method: String, parameters: T, type: U.Type, timeout: TimeInterval?, completion: @escaping (Result<U?, Error>) -> Void) {
         let request = Request(method: method, parameters: parameters)
         
         guard let id = request.id else {
@@ -71,8 +90,8 @@ public class Client: NSObject {
                 completion(.failure(ClientError.invalid(data: data, encoding: .utf8)))
                 return
             }
-
-            let timer = Timer.scheduledTimer(withTimeInterval: timeout, repeats: false) { timer in
+            
+            let timer = Timer.scheduledTimer(withTimeInterval: timeout ?? defaultInterval, repeats: false) { timer in
                 // Remove the receivable if the request hasn't received a response within the timeout.
                 if let index = self.receivableSubscribers.firstIndex(where: { $0.id == id }) {
                     self.receivableSubscribers.remove(at: index)
@@ -93,10 +112,10 @@ public class Client: NSObject {
                 if let index = self.receivableSubscribers.firstIndex(where: { $0.id == id }) {
                     // Invalidate the current timeout timer which is running.
                     self.receivableSubscribers[index].timer.invalidate()
-                        
+                    
                     // Remove the receivable once the request has been paired with a matching response.
                     self.receivableSubscribers.remove(at: index)
-                        
+                    
                     completion(.success(response.result))
                 }
             }
@@ -114,7 +133,7 @@ public class Client: NSObject {
         }
     }
     
-    public func subscribe<T: Codable>(to method: String, type: T.Type) throws {
+    func subscribe<T: Codable>(to method: String, type: T.Type) throws {
         // Only allow subscribing to a method once.
         guard notificationSubscribers.first(where: { $0.method == method }) == nil else {
             throw ClientError.duplicateSubscription
@@ -123,12 +142,12 @@ public class Client: NSObject {
         notificationSubscribers.append(NotificationSubscriber(method: method, completion: nil))
     }
     
-    public func on<T: Codable>(method: String, type: T.Type, completion: @escaping (T) -> Void) {
+    func on<T: Codable>(method: String, type: T.Type, completion: @escaping (T) -> Void) {
         if let index = notificationSubscribers.firstIndex(where: { $0.method == method }) {
             notificationSubscribers[index].completion = { data in
                 // Attempt to decode the data to a matching type.
                 let notification = try JSONDecoder().decode(Request<T>.self, from: data)
-
+                
                 // There's a chance that two methods point to one type.
                 guard self.notificationSubscribers[index].method == notification.method else {
                     return
@@ -139,12 +158,12 @@ public class Client: NSObject {
         }
     }
     
-    public func unsubscribe(from method: String) {
+    func unsubscribe(from method: String) {
         if let index = notificationSubscribers.firstIndex(where: { $0.method == method }) {
             notificationSubscribers.remove(at: index)
         }
     }
-
+    
     private func receive() {
         webSocketTask?.receive { result in
             switch result {
@@ -190,12 +209,13 @@ public class Client: NSObject {
     }
 }
 
-extension Client: URLSessionWebSocketDelegate {
-    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+//MARK: - URLSessionWebSocketDelegate
+extension ClientImpl: URLSessionWebSocketDelegate {
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
         connectCompletion?()
     }
     
-    public func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
         disconnectCompletion?()
     }
 }
