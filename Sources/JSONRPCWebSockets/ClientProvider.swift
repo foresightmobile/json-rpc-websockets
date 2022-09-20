e222d55d2f08d38d10a25a6c5d42a88c6b93ac0b
artemHrebinikChisw <75477796+artemHrebinikChisw@users.noreply.github.com>
2022-09-20 13:49:10 +0300

    added  ClientProvider, inject WebSockets to the client service

---
+++ Sources/JSONRPCWebSockets/ClientProvider.swift
@@ -0,0 +1,16 @@
+//
+//  ClientProvider.swift
+//  
+//
+//  Created by user on 20.09.2022.
+//
+
+import Foundation
+
+//MARK: - Client Interface
+public class Client {
+    public static func getClient() -> ClientProvider {
+        let webSocket = WebSocketBuilder.getWebSocketClient()
+        return ClientImpl(webSocketProvider: webSocket)
+    }
+}
